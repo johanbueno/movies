55425bd4ebcb0629049b40ac4a1a3b52c3c48995
Johan Bueno <johan.s.bueno@gmail.com>
2022-09-20 22:17:11 -0400

    posting the bid

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,8 +16,53 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createMovie();
+  // createMovie();
+  start();
 });
+
+function start() {
+  inquirer
+    .prompt({
+      name: "buyOrDonate",
+      type: "list",
+      message: "Would you like to buy a movie or Donate",
+      choices: ["BUY", "DONATE", "EXIT"],
+    })
+    .then(function (answer) {
+      if (answer.buyOrDonate === "BUY") {
+        buy();
+      } else if (answer.buyOrDonate === "DONATE") {
+        donate();
+      } else {
+        connection.end();
+      }
+    });
+}
+
+function buy() {
+  connection.query("SELECT * FROM movies", function (err, results) {
+    if (err) throw err;
+    inquirer.prompt([
+      {
+        name: "choice",
+        type: "list",
+        choices: function () {
+          var choiceArray = [];
+          for (var i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].title);
+          }
+          return choiceArray;
+        },
+        message: "Which movie would you like to buy?",
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "How much would you like to pay?",
+      },
+    ]);
+  });
+}
 
 function createMovie() {
   console.log("Inserting a new Movie....\n");
@@ -99,4 +145,3 @@ function readMovies() {
     connection.end();
   });
 }
-git;
